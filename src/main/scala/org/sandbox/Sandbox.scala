616f20d9b9cc0fc5265936f798fb6276803e41b8
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-10 22:04:36 +0200

    initial commit

---
+++ src/main/scala/org/sandbox/Sandbox.scala
@@ -0,0 +1,12 @@
+package org.sandbox
+
+class Sandbox {
+
+  def square(x: Int): Int = {
+    x * x
+  }
+
+  def multiple(x: Int, y: Int): Int = {
+    x * y
+  }
+}
---
+++ src/test/scala/org/sandbox/SandboxTest.scala
@@ -0,0 +1,16 @@
+package org.sandbox
+
+import junit.framework.Assert._
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+
+class SandboxTest extends JUnitSuite {
+  val samdbox = new Sandbox
+
+  @Test def samdboxText() {
+    assertEquals(1, samdbox square 1)
+    assertEquals(4, samdbox square 2)
+    assertEquals(9, samdbox square 3)
+    assertEquals(36, samdbox multiple(12, 3))
+  }
+}
